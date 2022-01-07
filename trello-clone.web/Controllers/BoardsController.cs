2c783f0fba47cf0ae3626265a8da73c78942e90a
Harry Trinh <40319325+harrytrinh123@users.noreply.github.com>
2022-01-07 17:14:20 +0700

    [Write] Add backgroundImage for board

--- trello-clone.web/Controllers/BoardsController.cs
+++ trello-clone.web/Controllers/BoardsController.cs
@@ -35,18 +35,7 @@ namespace trello_clone.web.Controllers
         public async Task<IActionResult> Read(string id)
         {
             var boardInfo = await _boardService.GetBoard(id, trelloToken);
-            //ViewBag.BoardInfo = boardInfo;
-
-            //var listsOfBoard = await _boardService.GetListsOfBoard(id, trelloToken);
-            //ViewBag.ListsOfBoard = listsOfBoard;
-
-            //List<List<Card>> lstCards = new List<List<Card>>();
-            //foreach (var item in listsOfBoard) {
-            //    var cardsOfList = await _listService.GetCardsOfList(item.id, trelloToken);
-            //    lstCards.Add(cardsOfList);
-            //}
-            //ViewBag.LstCards = lstCards;
-
+            ViewBag.BoardInfo = boardInfo;
             ViewBag.BoardId = boardInfo.id;
             ViewBag.Token = trelloToken;
             return View();
